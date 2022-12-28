8a09d55126cb6be5fec468b8642db4fd3c54eea3
Fer-Jgu <fernagur@gmail.com>
2022-12-27 18:04:49 -0600

    a bunch of things... again LOL

--- tree.py
+++ tree.py
@@ -15,6 +15,11 @@ from typing import List
 import ursina as u
 from ursina.shaders import lit_with_shadows_shader
 
+
+import subprocess
+from os import path
+from requests import get as gimmeMyRequest
+
 # I don't want to input my FPS every time I test my program so I do this for later use (:
 global test_path
 test_path = mypath().lower().startswith("C:\\Users\\inicio".lower())
@@ -26,9 +31,19 @@ global res_h
 
 global step
 global items
+global app
 global g_lights
 global g_spheres
 global counter
+
+global icon_file
+icon_file = "icon.ico"
+global bg_music_name
+global bg_music
+bg_music_name = "Best_Chrimu_Song.mp3"
+global ursified
+ursified = False
+
 step = 0
 items = []
 g_lights = []
@@ -67,12 +82,88 @@ class Utils():
             t += stepSize
         return positions
 
-class UrsinaUltimateTree():
-    def update():
+
+# Ursina Stuff
+# Ursina updates
+def update():
+    if ursified:
         global counter
         counter +=1
         random.choice(g_lights).scale = 0 + 4 * u.sin(counter * 0.005)
         random.choice(g_spheres).scale = 0 + 1 * u.sin(counter * 0.005)
+
+def exCiting():
+    print("Bye bye")
+
+class UrsinaUltimateTree():
+    def download_music(self):
+        global bg_music_name
+        bg_music_name = "Best_Chrimu_Song.mp3"
+        
+        download_stuff = not path.exists(bg_music_name)
+        # If there is no mp3 file we should just avoid requests and other processes, right?
+        print("Getting the most Chrismas-y song an AI could find...")
+        if download_stuff:
+            url = "https://cdn.trendybeatz.com/audio/Mariah-Carey-All-I-Want-For-Christmas-Is-You-(TrendyBeatz.com).mp3"
+            download = gimmeMyRequest(url)
+        
+        # All utility names
+        initial_output_name = "fix_" + bg_music_name.replace(".mp3",".wav")
+        fixed_output_name = initial_output_name.replace("fix_","")
+        bg_music_name = fixed_output_name
+
+        # Actually download the song, but in a mp3 format, so let's fix that later...
+        if download_stuff:
+            with open(bg_music_name, "wb") as f:    f.write(download.content)
+
+        # Make song a wav file
+        subprocess.run(['ffmpeg', '-y', '-i', bg_music_name, initial_output_name], 
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        # Cut the wav file from the first 7.5 seconds because the download site has some annoying ad through that bit
+        subprocess.run(['ffmpeg', '-y', '-i', initial_output_name, '-ss', '00:00:07.5', '-c', 'copy', fixed_output_name], 
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    
+    def download_icon(self):
+        download_stuff = not path.exists(icon_file)
+        
+        print("Getting icon file...")
+        if download_stuff:
+            url = "https://cdn3d.iconscout.com/3d/premium/thumb/python-6815592-5602757.png"
+            req = gimmeMyRequest(url)
+            with open(icon_file, "wb") as file:
+                file.write(req.content)
+        icoed_icon = icon_file.replace(".png",".ico")
+        print(f"{icoed_icon} {not path.exists(icoed_icon)}")
+        if not path.exists(icoed_icon):
+            subprocess.run(['convert', icon_file, icoed_icon])
+
+    def play_music(self):
+        global bg_music_name
+        global bg_music
+        bg_music = u.Audio(
+                bg_music_name,
+                volume=.2,
+                loop = True,
+                autoplay = True,
+                enabled = [])
+
+    def setSystemIcon(self):
+        self.download_icon()
+        u.window.icon = icon_file
+
+
+    def setAppData(self):
+        self.download_music()
+        self.setSystemIcon()
+        u.window.title = "Pythonistree!"
+        
+    def __init__(self) -> None:
+        global app
+
+        self.setAppData()
+        app = u.Ursina(development_mode=not test_path, fullscreen=False, borderless = False)
+        u.window.exit_button.visible = True
+        u.window.exit_button.on_click = exCiting
 
     def make_ursina_arrows(self):
         arrow_x = u.Entity(model="arrow", color=u.color.red, x=0.5)
@@ -102,8 +193,6 @@ class UrsinaUltimateTree():
         curve_res = 400
         
         while step < 2: print("Waiting for step...")
-
-        app = u.Ursina()
 
         if test_path: self.make_ursina_arrows()
         log = u.Entity(model=u.Cylinder(resolution=curve_res, radius=.5, start=0, height=5.6, direction=(0,1,0)#)) I tested and fixed a bug in ursina's repo while doing this part LOL
@@ -156,8 +245,12 @@ class UrsinaUltimateTree():
         # sky = u.Sky(texture="sky_sunset")
         ed = u.EditorCamera()
         ed.y = ed.y+10
+        ed.z = ed.z-30
+        ed.look_at(ground)
         # ed.z = ed.z-200
-        app.run()
+        self.play_music()
+        app.run(info=False)
+
 
 
 class MehTree():
@@ -220,7 +313,7 @@ class MehTree():
 			    background-color:var(--chrs-red);
 			}}
 			div.title, div.caption{{
-			    width: {w};
+			    width: {res_w};
 			    height: inherit;
 			}}
 			h1.title, div.caption {{
@@ -338,7 +431,6 @@ class MehTree():
                 ndot = points(pos=[vector(x,pos_y,z)], radius=0.3, size_units="world", color=set_color)
                 dot_levels.append(ndot)
         itera = 0
-        ursined = False
         global step
         print("\n\nNow look at your web browser ;)")
         ursination = UrsinaUltimateTree()
@@ -353,6 +445,7 @@ class MehTree():
             if step < 2:
                 input("Done? Press enter")
                 step = 2
+                ursified = True
                 await ursination.ursina_tree()
 
 class SuperAwesomeAndComplexTree():
@@ -389,7 +482,6 @@ if __name__ == "__main__":
     
     HyperComplex = SuperAwesomeAndComplexTree()
     SimpleTree = MehTree()
-    Meeeeh = UrsinaUltimateTree()
     HyperComplex.make_tree()
 
     if not test_path:
