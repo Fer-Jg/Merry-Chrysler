24176c2fc9350baa91210d1296ebaddb22f89608
Fer-Jgu <fernagur@gmail.com>
2022-12-27 20:47:20 -0600

    Moved files and fixed code for runner due to move

--- i_used_this_while_coding/__runner.py
+++ i_used_this_while_coding/__runner.py
@@ -1,16 +1,20 @@
 import re
 import os
 
+style_path = "i_used_this_while_coding/style.css"
+tree_path = "tree.py"
+
+
 def restyle():
     # Load current script ergo current style
-    with open("tree.py", "r") as tree_file:
+    with open(tree_path, "r") as tree_file:
         tree = tree_file.read()
         # new_text = re.sub(r'.*def __init__\(self\) -> None:', 'Start:', text, flags=re.DOTALL)
         # new_text = re.sub(r'def learning_vpython\(self\):.*', 'The End.', new_text, flags=re.DOTALL)
         tree_file.close()
     
     # Load new style
-    with open("style.css", "r") as style_file:
+    with open(style_path, "r") as style_file:
         style_lines = style_file.readlines()
         style = ""
         for line in style_lines: style+= "\t\t\t" + line
@@ -25,7 +29,7 @@ def restyle():
     style = re.sub(r'<style>.*</style>', f'<style>\n{style}\n\t\t</style>', tree, flags=re.DOTALL)
 
     print("Fixed style, writting to tree file...")
-    with open("tree.py", "w") as tree_file:
+    with open(tree_path, "w") as tree_file:
         tree_file.write(style)
         tree_file.close()
 
@@ -34,6 +38,6 @@ def run(new_style : bool):
         if new_style:
             restyle()
         
-        os.system("py tree.py")
+        os.system(f"py {tree_path}")
 
 run(True)
