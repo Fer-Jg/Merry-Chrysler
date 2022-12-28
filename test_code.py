6c0b6f35f30f351796446b6533c08eab9635f9fc
Fer-Jgu <fernagur@gmail.com>
2022-12-27 20:15:36 -0600

    did something here i guess, it was just testing tho

--- test_code.py
+++ test_code.py
@@ -8,9 +8,9 @@ app = Ursina()
 window.color = color.black
 
 from ursina.shaders import basic_lighting_shader
-tree = Entity(model=Cone(), color='#1d591e', scale=3, shader=basic_lighting_shader)
-tree.model.generate_normals()
-fake_light = Entity(model='quad', texture='radial_gradient', parent=tree, y=1, billboard=True, color=color.yellow, render_queue=1, alpha=.5)
+# tree = Entity(model=Cone(), color='#1d591e', scale=3, shader=basic_lighting_shader)
+# tree.model.generate_normals()
+fake_light = Entity(model='quad', texture='radial_gradient', parent=None, y=1, billboard=True, color=color.yellow, render_queue=1, alpha=.5)
 
 counter = 0
 def update():
@@ -19,19 +19,36 @@ def update():
     fake_light.scale = 0 + 2 * sin(counter * 0.005)
 
 
+import subprocess
+from os import path
+from requests import get as gimmeMyRequest
+
+global icon_file
+icon_file = "icon.ico"
+def download_icon(self):
+    download_stuff = not path.exists(icon_file)
+    
+    if download_stuff:
+        url = "https://cdn3d.iconscout.com/3d/premium/thumb/python-6815592-5602757.png"
+        req = gimmeMyRequest(url)
+        with open(icon_file, "wb") as file:
+            file.write(req.content)
+    icoed_icon = icon_file.replace(".png",".ico")
+    print(f"{icoed_icon} {not path.exists(icoed_icon)}")
+    if not path.exists(icoed_icon):
+        subprocess.run(['convert', icon_file, icoed_icon])
+
+global bg_music_name
+global bg_music
 def download_music(self):
     global bg_music_name
     bg_music_name = "Best_Chrimu_Song.mp3"
-
-    import subprocess
-    from os import path
     
     download_stuff = not path.exists(bg_music_name)
     # If there is no mp3 file we should just avoid requests and other processes, right?
     if download_stuff:
-        from requests import get as getMeMaMusic
         url = "https://cdn.trendybeatz.com/audio/Mariah-Carey-All-I-Want-For-Christmas-Is-You-(TrendyBeatz.com).mp3"
-        download = getMeMaMusic(url)
+        download = gimmeMyRequest(url)
     
     # All utility names
     initial_output_name = "fix_" + bg_music_name.replace(".mp3",".wav")
@@ -58,7 +75,7 @@ def play_music(self):
             loop = True,
             autoplay = True,
             enabled = [])
-    
+
 # music_t = 0
 # def input(key):
 #     global bg_music
@@ -72,7 +89,13 @@ def play_music(self):
 #             bg_music.play(start=music_t)
 
 download_music(0)
-play_music(0)
+# play_music(0)
+download_icon(0)
+
+# Blender object
+# blend = u.load_model("chrimu_star.blend")
+# obj = u.Entity(model=blend, color=u.color.green, shader=basic_lighting_shader)
+
 
 EditorCamera()
 
